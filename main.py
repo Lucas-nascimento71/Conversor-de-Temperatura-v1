613f65d700741ae26103d63f72425b82c4108f46
Lucas-nascimento71 <lnfsouza@hotmail.com>
2024-10-08 12:54:04 -0300

    parte 1

---
+++ main.py
@@ -0,0 +1,7 @@
+print('=-==-=-=-=-=-=-=-=-=CONVESOR DE TEMPERATURA=-=-=-==-=-=--=-=-=-=-=')
+
+unidade = str(input(('Em qual unidade de teperatura você irá usar [Cc/Ff/Kk]: ')))
+
+if unidade == 'c':
+    print('Celsius')
+    float(input(('Digite a temperatura: ')))
