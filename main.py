4aa27f555612fbee3c55e4acd855a2a840243564
Lucas-nascimento71 <lnfsouza@hotmail.com>
2024-10-08 13:08:23 -0300

    v1

--- main.py
+++ main.py
@@ -18,5 +18,17 @@ def kelvin_para_celsius(k):
 def kelvin_para_fahrenheit(k):
     return (k - 273.15) * 9/5 + 32
 
-unidade = str(input(('Em qual unidade de teperatura você irá usar [c/f/k]: ')))
-temperatura = float(input('Digite a temperaura: '))
+temperatura = float(input("Digite a temperatura: "))
+unidade = input("Digite a unidade (C, F, K): ").upper()
+
+if unidade == "C":
+    print(f"{temperatura}°C em Fahrenheit é {celsius_para_fahrenheit(temperatura)}°F")
+    print(f"{temperatura}°C em Kelvin é {celsius_para_kelvin(temperatura)}K")
+elif unidade == "F":
+    print(f"{temperatura}°F em Celsius é {fahrenheit_para_celsius(temperatura)}°C")
+    print(f"{temperatura}°F em Kelvin é {fahrenheit_para_kelvin(temperatura)}K")
+elif unidade == "K":
+    print(f"{temperatura}K em Celsius é {kelvin_para_celsius(temperatura)}°C")
+    print(f"{temperatura}K em Fahrenheit é {kelvin_para_fahrenheit(temperatura)}°F")
+else:
+    print("Unidade inválida!!!")
